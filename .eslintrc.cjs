3d3423590f7bbe5fb97089ce71522f0c952ad3e7
Navin Narayanan <nan.navinnarayanan@gmail.com>
2024-03-21 14:58:40 -0500

    setup vercel and github CI/CD

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,14 @@ const config = {
     "plugin:@typescript-eslint/stylistic-type-checked"
   ],
   "rules": {
+    "@typescript-eslint/no-unsafe-assignment": "warn",
+    "@typescript-eslint/no-unsafe-argument": "warn",
+    "@typescript-eslint/no-unsafe-member-access": "warn",
+    "@typescript-eslint/no-unsafe-call": "warn",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "prefer-const": "warn",
+    "@typescript-eslint/no-extra-semi": "warn",
+    
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/consistent-type-imports": [
