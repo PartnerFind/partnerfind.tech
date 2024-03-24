7fddd1e9f251ffb927491f42c1c57c2b17c08844
richp16 <patelrachit1612@gmail.com>
2024-03-23 19:41:35 -0500

    Tavily query post request with trimmed query

--- src/app/api/rag/route.ts
+++ src/app/api/rag/route.ts
@@ -1,17 +1,45 @@
 import { NextResponse } from 'next/server';
 import { NextApiResponse } from 'next';
+import { env } from "@/env"
 
 export async function POST(request: Request, res: NextApiResponse) {
     const data: any = await request.json();
     let testData = {
         "business_name": "Starbucks",
-        "address": "125 Plaza Dr, Wildwood, MO 63040"
+        "zip_code": "63040"
     }
     // First thing, call tavily api and pass in data
+    let tavilyQuery = `I want to partner with: ${testData.business_name} at this zip code ${testData.zip_code}. list me the contact info: phone number and email. Then tell me the abbr. type: (NPO:non profit org, FPO:for profit org, GA:Government Association, LB:Local Business, CB:Corporate Business). Then tell me the industry it is in, and a brief description and the resources they could provide to a high school.`
+    
+    const tavilyAPI = await fetch('https://api.tavily.com/search', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          api_key: process.env.TAVILY_API_KEY,
+          query: tavilyQuery,
+          search_depth: 'basic',
+          include_images: false,
+          include_answer: true,
+          include_raw_content: false,
+          max_results: 1,
+        }),
+      });
 
+      
+
+      if (tavilyAPI.ok) {
+        // Pass tavily response into groq
+      } else {
+        return new Response(`Failed to query tavily`, {
+            status: 500,
+        })
+      }
+
+      const tavilyResponse = await tavilyAPI.json();
     
 
-    // Pass tavily response into groq
 
     
     return NextResponse.json({ status: "success" });
