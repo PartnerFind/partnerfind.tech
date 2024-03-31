57f64736934dde4839d83529b551dfd9bce1ceb1
Navin Narayanan <67123306+navincodesalot@users.noreply.github.com>
2024-03-30 22:27:42 -0500

    Update route.ts

--- src/app/api/rag/route.ts
+++ src/app/api/rag/route.ts
@@ -20,7 +20,7 @@ export async function POST(request: Request, res: NextApiResponse) {
           include_images: false,
           include_answer: true,
           include_raw_content: false,
-          max_results: 1,
+          max_results: 5, // trying this out
         }),
       });
         
