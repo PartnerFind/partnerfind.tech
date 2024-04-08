fd896fb61e8c3991141453ac59fb985233a9ab94
richp16 <patelrachit1612@gmail.com>
2024-04-07 22:07:24 -0500

    temp rag fix

--- src/app/api/rag/route.ts
+++ src/app/api/rag/route.ts
@@ -6,9 +6,6 @@ export async function POST(request: Request, res: NextApiResponse) {
     const data: any = await request.json();
     // First thing, call tavily api and pass in data
     let tavilyPrompt = `I want to partner with: ${data.business_name} at this zip code ${data.zip_code}. list me the phone number and email,tell me the abbr. type: (NPO:non profit org, FPO:for profit org, GA:Government Association, LB:Local Business, CB:Corporate Business) tell me the industry it is in, brief description, and resources they could provide to a high school.Remove all the \\n and extra spaces from the response`
-   
-    console.log(tavilyPrompt)
-    console.log(tavilyPrompt.length)
 
     try {
       const tavilyAPI = await fetch('https://api.tavily.com/search', {
@@ -34,45 +31,41 @@ export async function POST(request: Request, res: NextApiResponse) {
         const groq = new Groq();
 
         let groqPrompt = `I am an administrator at my high school and need insightful information on potential community partners near me. 
-          I have used tavily API to scrape and find organizations and their info for me. I want you to take this info and compile it into JSON struture, 
-      with the final key in the array being all of the info compiled into a nicely and neatly formatted analysis page for my website. 
+          take the info I will give you from a tavilyAPI search and compile it into JSON struture, 
           I want you to give a detailed analysis on the resources that the organization can provide that you will be given, and also come up with and infer potential reasons to or 
       not to partner with this organization and how it will be beneficial for the school. 
           Furthemore, come up with how we can work with the organization such as giving a process (directions) by contacting the specific number and email, reaching out to them, 
       and then talking about how we can acquire their resources for our school. 
-          here is the tavilyAPI completion:  
-          "answer": "${tavilyResponse.answer}"  make sure to remove the extra \n and other discrepencies If for some reason you can't do something, omit the little apology statement 
-      from the text as I want the response to go straight to the static site and look as professional and consistently generated as possible. 
-          Make absolute sure your response is in this EXACT JSON format, with the array and key: value structure, since we will be storing some important info, so make the response in 
-      this EXACT array format (key:value):
-      the genpage data value below should have no \n or extra spaces, just a block of text that is formatted perfectly for a website
+          here is the tavilyAPI completion data:  
+          "answer": "${tavilyResponse.answer}"  
+          
+          I want the response to go straight to the static site and look as professional and consistently generated as possible. 
+          
+          Make absolute sure your response is in the following JSON format without any escape sequences like new lines and the first key should be phoneNumber.
+          Your response should be valid JSON string that is usable outside of the box without any tweaks for data.
+          make sure to remove any new lines and extra spaces from the JSON response.
           {
-          "phonenumber": "(format: (314)-292-6262 --- include parenthesis for area code and dashes between each area code, telephone prefix, and line number including a dash after 
-      the parenthesis)"
-          "email": "(whatever the email is)"
-          "name": "(exact business name from Tavily completion)"
-          "type": "(the industry it is in --- one worder, make sure it is exactly a singular word and make it very vague/general, simply saying the umbrella industry it's in)"
-          "resources": "(very brief list of resources they provide)"
-          "description": "(extremely brief and concise description of the organization)"
-          "category": "(3 letter abbreviation for the type of organization it is)"
+          "phoneNumber": "(format: (314)-292-6262 --- include parenthesis for area code and dashes between each area code, telephone prefix, and line number including a dash after 
+      the parenthesis)",
+          "email": "(whatever the email is)",
+          "name": "(exact business name from Tavily completion)",
+          "type": "(the industry it is in --- one worder, make sure it is exactly a singular word and make it very vague/general, simply saying the umbrella industry it's in)",
+          "resources": "(very brief list of resources they provide)",
+          "description": "(extremely brief and concise description of the organization)",
+          "category": "(3 letter abbreviation for the type of organization it is)",
           "genpage": {
             "summary": "(summary of the organization and who they are)",
             "resources": "(resources offered by the organization for highschools (be descriptive in your analysis))",
-            "reasons": "(list out reasons we as a high school should partner with the organization and the opportunities they offer)"
-            "flaws" : "(list out a couple reasons to reconsider our partnership with potential flaws)"
+            "reasons": "(list out reasons we as a high school should partner with the organization and the opportunities they offer)",
+            "flaws" : "(list out a couple reasons to reconsider our partnership with potential flaws)",
             "process" : "(list out directions and the process I should follow to partner with this organization)"
             }
-          }
- 
-          Do not put any text outside of the array, simply give me the EXACT JSON array that I am asking for with an extensively detailed analysis for the genpage nest JSON 
-      in which you will not put any divisions within the key values, making the values a continous string/block of text and thought.`;
+          }`
 
           const groqParams = {
             messages: [
-              {
-                  role: "user",
-                  content: groqPrompt
-              }
+              { role: 'system', content: 'You are a RAG assistant that only answers in valid JSON (no generation key just straight into what the prompt asks for)' },
+              { role: "user", content: groqPrompt }
             ],
             model: "mixtral-8x7b-32768",
             // temperature: "0.1", // TODO: Add these settings
