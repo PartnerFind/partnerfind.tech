1c940c7c6d31638b2f428efacbbd8f1c1fc14f42
Navin Narayanan <nan.navinnarayanan@gmail.com>
2024-04-12 16:56:48 -0500

    make data-table pagination and truncated descriptions

--- src/app/(main_app)/explore/explore-table.tsx
+++ src/app/(main_app)/explore/explore-table.tsx
@@ -1,33 +1,14 @@
 'use client'
 
-import { DataTable } from "@/components/table/data-table";
-import { columns } from "@/components/table/columns";
+import { DataTable } from "@/components/table/data-table-pagination";
+import { ColumnsPartner, columns } from "@/components/table/columns";
 import { useState, useEffect } from "react";
-
-export type FetchResponseDef = {
-  data: {
-    category: string
-    name: string
-    type: string
-    description: string
-    resources: string
-    phonenumber: string
-    email: string
-    genpage: {
-      summary: string
-      resources: string
-      reasons: string
-      flaws: string
-      process: string
-    }
-  }[];
-}
 
 async function fetchData(): Promise<any[]> {
   try {
     const response = await fetch(`/api/db/fetch-all-rows`);
     if (response.ok) {
-      const info: FetchResponseDef = await response.json();
+      const info: ColumnsPartner = await response.json();
       return info?.data ?? [];
     } else {
       throw new Error("Error fetching rows.");
@@ -40,7 +21,7 @@ async function fetchData(): Promise<any[]> {
 }
 
 export default function ExploreTable() {
-  const [data, setData] = useState<any[]>([]);
+  const [data, setData] = useState<ColumnsPartner[]>([]);
 
   useEffect(() => {
     async function fetchDataAndUpdateState() {
@@ -60,7 +41,7 @@ export default function ExploreTable() {
 
   return (
     <div className="container mx-auto py-10">
-      <DataTable columns={ columns } data={ data } />
+      <DataTable columns={columns} data={data} />
     </div>
   );
 }
--- src/components/table/columns.tsx
+++ src/components/table/columns.tsx
@@ -4,21 +4,22 @@ import { ColumnDef } from "@tanstack/react-table"
 
 // This type is used to define the shape of our data.
 export type ColumnsPartner = {
-  category: string
-  name: string
-  type: string
-  description: string
-  resources: string
-  phonenumber: string
-  email: string
-  genpage: {
-    summary: string
+  data: {
+    category: string
+    name: string
+    type: string
+    description: string
     resources: string
-    reasons: string
-    flaws: string
-    process: string
-  }
-  add_partner: boolean
+    phonenumber: string
+    email: string
+    genpage: {
+      summary: string
+      resources: string
+      reasons: string
+      flaws: string
+      process: string
+    }
+  }[]
 }
 
 export const columns: ColumnDef<ColumnsPartner>[] = [
@@ -37,6 +38,25 @@ export const columns: ColumnDef<ColumnsPartner>[] = [
   {
     accessorKey: "description",
     header: "Description",
+    cell: ({ row }) => {
+      let description: string = row.getValue("description");
+      if (description.length > 200) {
+        // If description is more than 200 characters, truncate and add an ellipise (...), but make sure to not slice on spaces
+        let truncatedDescription = description.slice(0, 197); 
+        let lastSpaceIndex = truncatedDescription.lastIndexOf(' ');
+  
+        if (lastSpaceIndex !== -1) {
+          // If a space was found before the 197th character, truncate at that space
+          return truncatedDescription.slice(0, lastSpaceIndex) + "...";
+        } else {
+          // If no space was found, truncate at the 197th character
+          return truncatedDescription + "...";
+        }
+      } else {
+        // Return the full description if it is less than or equal to 200 characters
+        return description;
+      }
+    },
   },
   {
     accessorKey: "resources",
@@ -45,10 +65,33 @@ export const columns: ColumnDef<ColumnsPartner>[] = [
   {
     accessorKey: "phonenumber",
     header: "Phone Number",
+    size: 200,
+    cell: ({ row }) => {
+      let phonenumber: string = row.getValue("phonenumber");
+      if (!phonenumber) {
+        return null;
+      } else {
+        return phonenumber;
+      }
+    },
   },
   {
     accessorKey: "email",
     header: "Email",
+    cell: ({ row }) => {
+      let email: string = row.getValue("email");
+      if (!email) {
+        return null;
+      } else {
+        email = email.toLowerCase();
+
+        return (
+          <a href={`mailto:${email}`} style={{ color: "#1a73e8", textDecoration: "underline" }}>
+            {email}
+          </a>
+        );
+      }
+    },
   },
   {
     accessorKey: "add_partner", // todo
