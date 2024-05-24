66baac0db7371c3fd108da55832523c66c29d3a5
RishiMadhavan-ui <rkmadhavan109@gmail.com>
2024-05-23 23:34:30 -0500

    home page formatting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,29 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import { twMerge } from "tailwind-merge";
-import { Navbar } from "@/components/navbar";
-import { SidebarNav } from "@/components/sidebar"
-import "@/styles/globals.css";
+import { ClerkProvider } from '@clerk/nextjs';
+import { twMerge } from 'tailwind-merge';
+import { Navbar } from '@/components/navbar';
+import { SidebarNav } from '@/components/sidebar';
+import '@/styles/globals.css';
 
-import { fontSans } from "@/lib/fonts"
+import { fontSans } from '@/lib/fonts';
 
 export const metadata = {
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  icons: [{ rel: 'icon', url: '/favicon.ico' }],
 };
 
 const sidebarNavItems = [
   {
-    title: "Explore Partners",
-    href: "/explore",
+    title: 'Explore',
+    href: '/explore',
   },
   {
-    title: "My List",
-    href: "/my-list",
+    title: 'My List',
+    href: '/my-list',
   },
   {
-    title: "Add Partner",
-    href: "/add-partner",
+    title: 'Add',
+    href: '/add-partner',
   },
-]
+];
 
 export default function RootLayout({
   children,
@@ -33,20 +33,19 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={twMerge(
-            "min-h-screen bg-background font-sans antialiased",
+        <body
+          className={twMerge(
+            'min-h-screen bg-background font-sans antialiased',
             fontSans.variable
-          )}>
-          <Navbar/> 
-          <div style={{ display: 'flex'}}>
-            <aside className="absolute z-10 w-1/5 h-full flex flex-col items-start justify-start"
-              style={{ marginLeft: '0.69%', marginTop: '19%'}}>
+          )}
+        >
+          <Navbar />
+          <div className="flex flex-col md:flex-row">
+            <aside className="md:absolute z-10 top-24 md:top-32 left-0 w-full md:w-1/5">
               <SidebarNav items={sidebarNavItems} />
             </aside>
+            <main className="flex-1">{children}</main>
           </div>
-          <main style={{ flex: 1 }}>
-            {children}
-          </main>
         </body>
       </html>
     </ClerkProvider>
