3e3fe0173573d6d646d22bfee85bcc5145c42d1e
RishiMadhavan-ui <rkmadhavan109@gmail.com>
2024-06-28 00:26:49 -0500

    finish

--- src/app/(main_app)/my-list/template.tsx
+++ src/app/(main_app)/my-list/template.tsx
@@ -1,16 +1,16 @@
-'use client'
+"use client";
 
-import { useSearchParams } from 'next/navigation'
-import { ReactNode, Suspense } from 'react'
+import { useSearchParams } from "next/navigation";
+import { ReactNode, Suspense } from "react";
 
-import Loading3Dots from '@/components/Loading3Dots'
+import Loading3Dots from "@/components/Loading3Dots";
 
 export default function Template({ children }: { children: ReactNode }) {
-  const searchParams = useSearchParams()
+  const searchParams = useSearchParams();
 
   return (
     <Suspense fallback={<Loading3Dots />} key={searchParams.toString()}>
       {children}
     </Suspense>
-  )
+  );
 }
