597b68ae21df2f38b6e594a0fac0e3fca02bd001
Navin Narayanan <nan.navinnarayanan@gmail.com>
2024-06-28 21:48:05 -0500

    fix favicon

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher([
   "/",
   "/legal(.*)",
+  "/favicon.ico",
   "/my-list/(.*)", // anyone can see /my-list/[slug], but not /my-list without auth
   "/api(.*)",
 ]);
